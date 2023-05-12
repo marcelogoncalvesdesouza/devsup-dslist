30bdfe5c72b6525ee5f9171cb7ce30b28935230a
Marcelo Gonçalves de Souza <marcelo.souza@dnweb.com.br>
2023-05-12 18:36:37 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -1,7 +1,37 @@
 package com.devsuperior.dslist.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
 
+    public GameList() {
+    }
 
+    public GameList(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
